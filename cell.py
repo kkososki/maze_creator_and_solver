21d2da333cb46d3f324f37cae5487c50e9cbdf93
Karla Barth <kkososki@gmail.com>
2024-06-13 14:46:35 -0300

    insert graphic and cells

---
+++ cell.py
@@ -0,0 +1,50 @@
+from tkinter import Y
+from graphic import Line, Point
+
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        if self._win is None:
+            return
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
+        if self.has_left_wall:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_top_wall:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_right_wall:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line)
+        if self.has_bottom_wall:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        half_len = abs(self._x2 - self._x1)//2
+        x_center = half_len + self._x1
+        y_center = half_len + self._y1
+
+        half_len2 = abs(to_cell._x2 - to_cell._x1)//2
+        x_center2 = half_len2 + to_cell._x1
+        y_center2 = half_len2 + to_cell._y1
+
+        fill_color = "red"
+        if undo:
+            fill_color = "gray"
+        
+        line = Line(Point(x_center, y_center), Point(x_center2, y_center2))
+        self._win.draw_line(line, fill_color)
---
+++ graphic.py
@@ -0,0 +1,40 @@
+from tkinter import Tk, BOTH, Canvas
+from turtle import width
+
+class Window:
+    def __init__(self, width, height):
+        self.__root = Tk()
+        self.__root.title("Maze Creator and Solver")
+        self.__canvas = Canvas(self.__root, bg="white", height=height, width=width)
+        self.__canvas.pack(fill=BOTH, expand=1)
+        self.__running = False
+        self.__root.protocol("WM_DELETE_WINDOW", self.close)
+
+    def redraw(self):
+        self.__root.update_idletasks()
+        self.__root.update()
+
+    def wait_for_close(self):
+        self.__running = True
+        while self.__running:
+            self.redraw()
+        print("window closed...")
+
+    def draw_line(self, line, fill_color="black"):
+        line.draw(self.__canvas, fill_color)
+
+    def close(self):
+        self.__running = False
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class Line:
+    def __init__(self, pi, pf):
+        self.pi = pi
+        self.pf = pf
+
+    def draw(self, canvas, fill_color="black"):
+        canvas.create_line(self.pi.x, self.pi.y, self.pf.x, self.pf.y, fill=fill_color, width=2)
